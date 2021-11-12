b6ca1622c4ba139a9b42a260d694a3b7e50acec3
Ashutosh Thakur <ashutoshth456@gmail.com>
2021-11-13 01:58:11 +0530

    initial commit

--- sponsro.js
+++ sponsro.js
@@ -44,6 +44,6 @@ app.use(function(req,res,next){
 app.use(signup);
 app.use(home);
 app.use(profile);
-app.listen("3000",process.env.IP,function(){
+app.listen(process.env.PORT,process.env.IP,function(){
     console.log("started");
 });
